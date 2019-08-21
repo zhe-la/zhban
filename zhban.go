3aa007c37b8a8670d1f033714df2c8b9ba2b5d53
Zhenya <zhenya1993lzn@Yandex.ru>
2019-08-21 19:43:57 +0300

    url remove from post, new tests

--- zhban.go
+++ zhban.go
@@ -1,15 +1,16 @@
 package zhban
 
 import (
-  "fmt"
-  "net/http"
-  "io/ioutil"
-  "time"
-  "github.com/corpix/uarand"
-  "golang.org/x/net/html/charset"
-  "flag"
-  "math/rand"
-  "strconv"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"math/rand"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/corpix/uarand"
+	"golang.org/x/net/html/charset"
 )
 
 const nginxError string = "<html><head><title>410 Gone</title></head><body bgcolor=\"white\"><center><h1>410 Gone</h1></center><hr><center>nginx/1.13.10</center></body></html>"
@@ -17,182 +18,154 @@ const nginxErrorCode int = http.StatusGone //410
 
 const errorCode int = http.StatusInternalServerError
 
-var tr = &http.Transport{
-  MaxIdleConns:       10,
-  IdleConnTimeout:    30 * time.Second,
-  DisableCompression: true,
-  TLSHandshakeTimeout: 0 * time.Second,
+type ClientData struct {
+	settings Settings
+	client   http.Client
 }
-var client = &http.Client{Transport: tr}
 
 type Settings struct {
-  proxyHeaders bool
-  browserHeaders bool
-  verbose bool
-  utf8 bool
-  keyParam string
-  port int
-  keyParamEnable bool
+	proxyHeaders      bool
+	browserHeadersGen bool
+	verbose           bool
+	toUtf8            bool
+	keyParam          string
+	port              int
+	keyParamEnable    bool
 }
 
 func rangeIn(low, hi int) string {
-    return time.Now().Format("2006-01-02 15:04:05")+" "+strconv.Itoa(low + rand.Intn(hi-low))
+	return time.Now().Format("2006-01-02 15:04:05") + " " + strconv.Itoa(low+rand.Intn(hi-low))
 }
 
-func (settings *Settings) GetData(w http.ResponseWriter, r *http.Request) {
-  Reqid := rangeIn(10000,99999)+" "
-  if (r.Method == "POST") {
-    if err := r.ParseForm(); err != nil {
-      w.WriteHeader(errorCode)
-      fmt.Fprintf(w, "ParseForm() err: %v", err)
-      return
-    }
+func (clientData *ClientData) GetData(w http.ResponseWriter, r *http.Request) {
+	Reqid := rangeIn(10000, 99999) + " "
+	if clientData.settings.keyParamEnable {
+		key := r.Header.Get("Key")
+		if key != clientData.settings.keyParam {
+			fmt.Println(Reqid + "Wrong key!")
+			w.Header().Set("Content-Type", "text/html; charset=utf-8")
+			w.WriteHeader(nginxErrorCode)
+			fmt.Fprintf(w, nginxError)
+			return
+		}
+	}
 
-    if (settings.keyParamEnable) {
-      key := r.FormValue("key")
-      if (key != settings.keyParam) {
-        fmt.Println(Reqid+"Wrong key!")
-        w.Header().Set("Content-Type", "text/html; charset=utf-8")
-          w.WriteHeader(nginxErrorCode)
-          fmt.Fprintf(w, nginxError)
-        return
-      }         
-    }
+	url := r.URL.String()
 
-    url := r.FormValue("url")
-    if(url=="") {
-      fmt.Println(Reqid+"No url param")
-      w.Header().Set("Content-Type", "text/html; charset=utf-8")
-        w.WriteHeader(nginxErrorCode)
-        fmt.Fprintf(w, nginxError)
-        return
-    }
+	if clientData.settings.verbose {
+		fmt.Println("\n" + Reqid + "NEW Request")
+		fmt.Println(Reqid+"URL:", url)
+		fmt.Printf(Reqid+"HOST: %s URL: %s RequestURI: %s Protocol version: %s TransferEncoding %q \n", r.Host, r.URL.Path, r.RequestURI, r.Proto, r.TransferEncoding)
+	}
 
-    if settings.verbose {
-      fmt.Println("\n"+Reqid+"NEW Request")
-      fmt.Println(Reqid+"URL:",url)
-      fmt.Printf(Reqid+"HOST: %s URL: %s RequestURI: %s Protocol version: %s TransferEncoding %q \n", r.Host, r.URL.Path,r.RequestURI,r.Proto,r.TransferEncoding)
-    }
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		fmt.Println(Reqid+"Request construction error", err.Error())
+		w.WriteHeader(errorCode)
+		fmt.Fprintf(w, "Request construction error")
+		return
+	}
 
-    req, err := http.NewRequest(http.MethodGet, url, nil)
-    if err != nil {
-        fmt.Println(Reqid+"Request construction error",err.Error())
-        w.WriteHeader(errorCode)
-          fmt.Fprintf(w, "Request construction error")
-        return
-    }
+	if clientData.settings.proxyHeaders {
+		req.Header = r.Header
+	}
 
-    if settings.proxyHeaders {
-      req.Header = r.Header
-    }
+	if clientData.settings.browserHeadersGen {
+		req.Header.Set("Host", req.Host)
+		req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+		req.Header.Set("Referer", url)
+		req.Header.Set("User-Agent", uarand.GetRandom())
+		req.Header.Set("Accept-Encoding", "*")
+		req.Header.Set("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
+	}
 
-    if settings.browserHeaders {
-      req.Header.Set("Host", req.Host)
-      req.Header.Set("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
-      req.Header.Set("Referer", url)
-      req.Header.Set("User-Agent", uarand.GetRandom())
-      req.Header.Set("Accept-Encoding", "*")
-      req.Header.Set("Accept-Language", "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7")
-    }
+	if clientData.settings.verbose {
+		fmt.Println(Reqid + "Headers to send:")
+		for k, v := range req.Header {
+			fmt.Printf(Reqid+"%q => %q\n", k, v[0])
+		}
+	}
 
-    if settings.verbose {
-      fmt.Println(Reqid+"Headers to send:");
-      for k, v := range req.Header {
-        fmt.Printf(Reqid+"%q => %q\n",k, v[0])
-      }
-    }
+	resp, err := clientData.client.Do(req)
+	if err != nil {
+		fmt.Println(Reqid+"Making request error:", err.Error())
+		w.WriteHeader(errorCode)
+		fmt.Fprintf(w, "Making request error")
+		return
+	}
 
-    resp, err := client.Do(req)
-    if err != nil {
-      fmt.Println(Reqid+"Making request error:",err.Error())
-      w.WriteHeader(errorCode)
-      fmt.Fprintf(w, "Making request error")
-      return
-    }
+	defer resp.Body.Close()
 
-    defer resp.Body.Close()
+	if clientData.settings.verbose {
+		fmt.Println(Reqid + "Response")
+		fmt.Printf(Reqid+"Status: %s StatusCode: %d Uncompressed: %t Protocol version: %s TransferEncoding %q \n", resp.Status, resp.StatusCode, resp.Uncompressed, resp.Proto, resp.TransferEncoding)
+	}
 
-    if settings.verbose {
-      fmt.Println(Reqid+"Response")
-      fmt.Printf(Reqid+"Status: %s StatusCode: %d Uncompressed: %t Protocol version: %s TransferEncoding %q \n", resp.Status, resp.StatusCode,resp.Uncompressed,resp.Proto,resp.TransferEncoding)
-    }
+	data := &resp.Body
 
-    data := &resp.Body
+	if clientData.settings.toUtf8 {
+		r, err := charset.NewReader(*data, resp.Header.Get("Content-Type"))
+		if err != nil {
+			fmt.Println(Reqid+"Reader encoding error:", err)
+			w.WriteHeader(errorCode)
+			fmt.Fprintf(w, "Reader encoding error")
+			return
+		}
+		rr := ioutil.NopCloser(r)
+		data = &rr
+	}
 
-    if settings.utf8 {
-      r, err := charset.NewReader(*data, resp.Header.Get("Content-Type"))
-      if err != nil {
-        fmt.Println(Reqid+"Reader encoding error:", err)
-        w.WriteHeader(errorCode)
-        fmt.Fprintf(w, "Reader encoding error")
-        return
-      }
-      rr := ioutil.NopCloser(r)
-      data = &rr
-    }
+	body, err := ioutil.ReadAll(*data)
+	if err != nil {
+		fmt.Println(Reqid+"Couldn't parse response body", err.Error())
+		w.WriteHeader(errorCode)
+		fmt.Fprintf(w, "Couldn't parse response body")
+		return
+	}
 
-    body, err := ioutil.ReadAll(*data);
-    if err != nil {
-      fmt.Println(Reqid+"Couldn't parse response body", err.Error())
-      w.WriteHeader(errorCode)
-      fmt.Fprintf(w, "Couldn't parse response body")
-      return
-    }
+	if clientData.settings.verbose {
+		fmt.Println(Reqid + "send result")
+	}
 
-    if settings.verbose {
-      fmt.Println(Reqid+"send result");
-    }
-
-    fmt.Fprint(w, string(body))
-  } else {
-    fmt.Println(Reqid+"NOT POST METHOD")
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    w.WriteHeader(nginxErrorCode)
-    fmt.Fprintf(w, nginxError)
-    return
-  }
+	fmt.Fprint(w, string(body))
 }
 
 func main() {
-  fmt.Println("")
-  fmt.Println("  |-| ")
-  fmt.Println("  | | ")
-  fmt.Println(" /   \\")
-  fmt.Println(" | Z |")
-  fmt.Println(" | H |")
-  fmt.Println(" | B |")
-  fmt.Println(" | A |")
-  fmt.Println(" | N |")
-  fmt.Println(" |___|")
-  fmt.Println("")
 
-  settings := &Settings{}
+	transport := &http.Transport{
+		MaxIdleConns:        10,
+		IdleConnTimeout:     30 * time.Second,
+		DisableCompression:  true,
+		TLSHandshakeTimeout: 0 * time.Second,
+	}
 
-  flag.BoolVar(&settings.proxyHeaders,"ph", false, "Enable proxing headers to final host")
-  flag.BoolVar(&settings.browserHeaders,"bh", true, "Generate browser headers to final host. User-Agent Header is a random UA")
-  flag.BoolVar(&settings.verbose,"v", false, "Verbose output")
-  flag.BoolVar(&settings.utf8,"utf8", false, "Convert output data to utf8 encoding")
-  flag.StringVar(&settings.keyParam,"k", "", "Security key. If not set - insecure connection")
-  flag.IntVar(&settings.port,"p", 3000, "Port for waiting connections with POST requests")
-  flag.Parse()
+	clientData := &ClientData{
+		settings: Settings{
+			keyParamEnable: false,
+		},
+		client: http.Client{Transport: transport},
+	}
 
-  // show options
-  fmt.Println("Proxy headers enable:", settings.proxyHeaders);
-  fmt.Println("Browser headers enable:", settings.browserHeaders);
-  fmt.Println("Verbose output:", settings.verbose);
+	flag.BoolVar(&clientData.settings.proxyHeaders, "ph", false, "Enable proxing headers to final host")
+	flag.BoolVar(&clientData.settings.browserHeadersGen, "bh", true, "Generate browser headers to final host. User-Agent Header is a random UA")
+	flag.BoolVar(&clientData.settings.verbose, "v", false, "Verbose output")
+	flag.BoolVar(&clientData.settings.toUtf8, "u", false, "Convert response data to utf8 encoding")
+	flag.StringVar(&clientData.settings.keyParam, "k", "", "Security key. If set, the request must contain the header \"Key\"")
+	flag.IntVar(&clientData.settings.port, "p", 3000, "Port for waiting connections")
+	flag.Parse()
 
-  settings.keyParamEnable = false
-  if (settings.keyParam != "") {
-    settings.keyParamEnable = true
-  }
-  fmt.Println("Key enable:",settings.keyParamEnable);
+	// show options
+	fmt.Println("Proxy headers enable:", clientData.settings.proxyHeaders)
+	fmt.Println("Browser headers enable:", clientData.settings.browserHeadersGen)
+	fmt.Println("Verbose output:", clientData.settings.verbose)
 
-  fmt.Println("");
+	if clientData.settings.keyParam != "" {
+		clientData.settings.keyParamEnable = true
+	}
+	fmt.Println("Key enable:", clientData.settings.keyParamEnable)
 
-  http.HandleFunc("/", settings.GetData)
-  fmt.Println("---------------------------------")
-  fmt.Println(" Sever listening on 0.0.0.0:"+strconv.Itoa(settings.port))
-  fmt.Println("---------------------------------")
-  fmt.Println("")
-  http.ListenAndServe(":"+strconv.Itoa(settings.port), nil)
+	http.HandleFunc("/", clientData.GetData)
+
+	fmt.Println("Sever listening on 0.0.0.0:" + strconv.Itoa(clientData.settings.port))
+	http.ListenAndServe(":"+strconv.Itoa(clientData.settings.port), nil)
 }
--- zhban_test.go
+++ zhban_test.go
@@ -1,78 +1,149 @@
 package zhban
 
 import (
-    "net/http"
-    "net/http/httptest"
-    "testing"
-    "strings"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
-func TestGetData(t *testing.T) {
-  settings := &Settings{keyParamEnable: true, keyParam:"123"}
-  /* 
-    check no url, key exist
-  */
-  req, err := http.NewRequest("POST", "/", strings.NewReader("key=123"))
-  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-  if err != nil {
-      t.Fatal(err)
-  }
-  rr := httptest.NewRecorder()
-  handler := http.HandlerFunc(settings.GetData)
-  handler.ServeHTTP(rr, req)
+func ServerTestRequests(w http.ResponseWriter, r *http.Request) {
+	ua := r.Header.Get("User-Agent")
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, ua)
+}
 
-  status := rr.Code
+func TestBroweserHeadersData(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(ServerTestRequests))
 
-  if status != http.StatusGone {
-      t.Errorf("handler returned wrong status code: got %v want %v",
-          status, http.StatusGone)
-  }
+	clientData := &ClientData{
+		settings: Settings{keyParamEnable: true, keyParam: "right_key"},
+	}
 
-  expected := nginxError
-  if rr.Body.String() != expected {
-      t.Errorf("handler returned unexpected body: got %v want %v",
-          rr.Body.String(), expected)
-  }
+	key := "right_key"
 
-  /* 
-    check wrong key
-  */
-  req, err = http.NewRequest("POST", "/", strings.NewReader("url=http://ya.ru&key=111"))
-  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-  if err != nil {
-      t.Fatal(err)
-  }
-  rr = httptest.NewRecorder()
-  handler = http.HandlerFunc(settings.GetData)
-  handler.ServeHTTP(rr, req)
+	/*
+	   check headers (bh not set)
+	*/
+	req, err := http.NewRequest("GET", ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Key", key)
 
-  status = rr.Code
-  if status != http.StatusGone {
-      t.Errorf("handler returned wrong status code: got %v want %v",
-          status, http.StatusGone)
-  }
+	handler := http.HandlerFunc(clientData.GetData)
 
-  expected = nginxError
-  if rr.Body.String() != expected {
-      t.Errorf("handler returned unexpected body: got %v want %v",
-          rr.Body.String(), expected)
-  }
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
 
-  /* 
-    check all
-  */
-  req, err = http.NewRequest("POST", "/", strings.NewReader("url=http://ya.ru&key=123"))
-  req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-  if err != nil {
-      t.Fatal(err)
-  }
-  rr = httptest.NewRecorder()
-  handler = http.HandlerFunc(settings.GetData)
-  handler.ServeHTTP(rr, req)
+	status := rr.Code
 
-  status = rr.Code
-  if status != http.StatusOK {
-      t.Errorf("handler returned wrong status code: got %v want %v",
-          status, http.StatusOK)
-  }
+	if status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected := "Go-http-client/1.1"
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
+	/*
+	   check headers (bh is set)
+	*/
+
+	clientData = &ClientData{
+		settings: Settings{keyParamEnable: true, keyParam: "right_key", browserHeadersGen: true},
+	}
+
+	req, err = http.NewRequest("GET", ts.URL, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Key", key)
+
+	handler = http.HandlerFunc(clientData.GetData)
+
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	status = rr.Code
+
+	if status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
+
+	expected = "Go-http-client/1.1"
+	if rr.Body.String() == expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
+	ts.Close()
+}
+
+func TestKey(t *testing.T) {
+
+	clientData := &ClientData{
+		settings: Settings{keyParamEnable: true, keyParam: "right_key"},
+	}
+
+	key := "wrong_key"
+
+	/*
+	   check wrong key
+	*/
+	req, err := http.NewRequest("GET", "http://ya.ru/123", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Key", key)
+
+	handler := http.HandlerFunc(clientData.GetData)
+
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	status := rr.Code
+
+	if status != http.StatusGone {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusGone)
+	}
+
+	expected := nginxError
+	if rr.Body.String() != expected {
+		t.Errorf("handler returned unexpected body: got %v want %v",
+			rr.Body.String(), expected)
+	}
+
+	/*
+	   check right key
+	*/
+
+	key = "right_key"
+
+	req, err = http.NewRequest("GET", "http://ya.ru/123", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Key", key)
+
+	handler = http.HandlerFunc(clientData.GetData)
+
+	rr = httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	status = rr.Code
+
+	if status != http.StatusOK {
+		t.Errorf("handler returned wrong status code: got %v want %v",
+			status, http.StatusOK)
+	}
 }
